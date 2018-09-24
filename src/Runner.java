79c71640775c3a7f85956ce7db3dd29b989b76cb
Sahil Mehta <sahilmehta17@gmail.com>
2018-09-24 18:02:07 -0400

    Add map to make expanding into multiple HandExtractors easier

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -6,8 +7,10 @@ class Runner {
     public static void main(String[] args) {
         Deck d = new Deck();
 
-        int num_trials = 10000;
+        Map<HandExtractorMap, Double> probabilities = new HashMap<>();
+        probabilities.put(HandExtractorMap.OnePairHandExtractor, 0.0);
 
+        int num_trials = 10000;
         for (int i = 1; i <= 52; i++) {
 
             double probability = 0;
@@ -21,15 +24,21 @@ class Runner {
                 Map<HandExtractorMap, Integer> collectorMap = hc.collect();
 
                 if (collectorMap.get(HandExtractorMap.OnePairHandExtractor) > 0) {
-                    probability += 1;
+                    probabilities.put(
+                        HandExtractorMap.OnePairHandExtractor,
+                        probabilities.get(HandExtractorMap.OnePairHandExtractor) + 1
+                    );
                 }
 
                 // Return the cards and shuffle for next deal
                 d.prepareForNext(dealt);
             }
 
-            probability /= num_trials;
-            System.out.println(i + ", " + probability);
+            probabilities.put(
+                HandExtractorMap.OnePairHandExtractor,
+                probabilities.get(HandExtractorMap.OnePairHandExtractor) / num_trials
+            );
+            System.out.println(i + ", " + probabilities.get(HandExtractorMap.OnePairHandExtractor));
         }
     }
 }
