857a6aced36940a70f7d9ffd11311537ca37db05
Sahil Mehta <sahilmehta17@gmail.com>
2018-09-24 16:41:45 -0400

    Implement OnePairHandExtractor

--- src/OnePairHandExtractor.java
+++ src/OnePairHandExtractor.java
@@ -1,11 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 class OnePairHandExtractor implements HandExtractor {
 
+    // Return the number of unique values for which there exist pairs
     public int extract(Set<Card> dealt) {
+        Map<CardValue, Integer> counts = new HashMap<>();
 
+        for (Card c : dealt) {
+            CardValue val = c.getValue();
 
-        return 5;
+            // Create key, value pair if it does not exist
+            if (!counts.containsKey(val)) {
+                counts.put(val, 0);
+            }
+
+            // Increment value
+            counts.put(val, counts.get(val) + 1);
+        }
+
+        // TODO: Decide if we want to distinguish between pairs of the same value
+        // Count all values that have at least two occurrences (one pair).
+        // Maximum of one pair counted per value
+        int num_paired_values = 0;
+        for (CardValue val : counts.keySet()) {
+            if (counts.get(val) >= 2) {
+                num_paired_values++;
+            }
+        }
+
+        return num_paired_values;
     }
 
 }
